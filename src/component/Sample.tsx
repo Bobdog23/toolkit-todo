64b30b82f884b2107bb027dcd91ddd2368c70674
Kouki <roto0213www@gmail.com>
2022-08-27 22:14:39 +0900

    sample作成

---
+++ src/component/Sample.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sample = () => {
+  return (
+    <>
+      <div>Sample</div>
+      <button onClick={() => console.log("buttonだよ")}>ボタン </button>
+    </>
+  );
+};
+
+export default Sample;
